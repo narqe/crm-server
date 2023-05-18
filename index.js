3388ef7c44aa0a80871c78e277dc173d5c9face2
Joel Acef <joelacef@gmail.com>
2023-05-18 19:32:17 -0300

    fix dots

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ const server = new ApolloServer({
 
 // start server
 
-server.listen(process.env.PORT || 4000).then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
     console.log(`Servidor listo: ${url} - ${process.env.NODE_ENV}`);
 })
