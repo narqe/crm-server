37bc649f20fd275fa62e84154716f9ba85ca3eaa
Joel Acef <joelacef@gmail.com>
2023-05-18 21:01:15 -0300

    log mongo

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ require('dotenv').config({ path: '.variables.env' });
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DB_MONGO)
+        console.log(process.env.DB_MONGO);
         console.log('DB Connected')
     } catch (error) {
         process.exit(1);
