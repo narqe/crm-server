a603dc3f7881f997f655c1b9d88bda4a07fc5ce7
Joel Acef <joelacef@gmail.com>
2023-05-18 21:42:30 -0300

    server changes

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 const connectDb = require('./config/db');
+const express = require('express');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.variables.env' });
 
-const server = new ApolloServer({
-    cors: {
-        origin: '*',			// <- allow request from all domains
-        credentials: true,
-        headers: true,
-    },
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-        const token = req.headers['authorization'] || '';
-        if (token) {
-            try {
-                const user = await jwt.verify(token.replace('Bearer ', ''), process.env.SECRET)
-                return {
-                    user
-                };
-            } catch (error) {
-                throw new Error('Invalid token');
+connectDb();
+async function startApolloServer() {
+
+    const app = express();
+
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: async ({ req }) => {
+            const token = req.headers['authorization'] || '';
+            if (token) {
+                try {
+                    const user = await jwt.verify(token.replace('Bearer ', ''), process.env.SECRET)
+                    return {
+                        user
+                    };
+                } catch (error) {
+                    throw new Error('Invalid token');
+                }
             }
         }
-    }
+    });
+
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    // start server
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`Servidor listo - ${process.env.NODE_ENV}`);
+    })
+}
+
+startApolloServer().catch((err) => {
+    console.error(err);
 });
-
-// start server
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`Servidor listo: ${url} - ${process.env.NODE_ENV}`);
-})
-
-connectDb();
